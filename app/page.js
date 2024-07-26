5cde93419f3846b9cbd901600093eb30ad4ce4b8
ajiaron <aaronjiang2001@gmail.com>
2024-07-26 14:09:46 -0700

    catalog section

--- app/page.js
+++ app/page.js
@@ -4,6 +4,16 @@ import React, {useState, useEffect, useRef} from 'react'
 import styles from "../styles/page.module.scss";
 import Logo from '../public/assets/logo.svg'
 import Navbar from "./components/Navbar";
+import Bvexterior from "../public/assets/bvexterior.png"
+import Mansion from "../public/assets/mansion.png"
+import Dwiw from "../public/assets/dwiw.png"
+import Poker from "../public/assets/trukamansion.png"
+import Peakingduck from "../public/assets/peakingduck.png"
+import Truka from "../public/assets/truka.png"
+import Tradesmark from "../public/assets/tradesmark.png"
+import Blackprint from "../public/assets/blackprint.png"
+
+
 export default function Home() {
   const [windowSize, setWindowSize] = useState({
     width: undefined,
@@ -50,7 +60,7 @@ export default function Home() {
               <p className={styles.heroHeaderSubtext} style={{marginBottom:".925rem"}}>
                 Building designs that inspire:
               </p>
-              <p className={styles.heroHeaderText}>
+              <h1 className={styles.heroHeaderText}>
                 If a blueprint is the&nbsp; foundation, the 
                 <span className={styles.heroHighlight}>
                   <p className={[styles.heroHeaderText, styles.heroHeaderTextAlt].join(' ')}>
@@ -58,7 +68,7 @@ export default function Home() {
                   </p>
                 </span> 
                 is everything else.&nbsp;
-              </p>
+              </h1>
               <div className={styles.heroButtonContainer}>
                 <span className={styles.heroStartButton}>
                   <p className={styles.buttonTextSmall}>
@@ -89,6 +99,42 @@ export default function Home() {
           <span className={styles.legacyIcon}/>
           <span className={styles.trukaIcon}/>
         </div>
+      </section>
+      <section className={styles.catalogSection}>
+        <div className={styles.sectionHeaderContainer}>
+          <p className={styles.sectionHeaderText}>
+            Designs By Blackprint
+          </p>
+          <p className={styles.subheaderText}>
+            From websites to graphics, we make sure your project stands out from the rest.
+          </p>
+        </div>
+
+        <div className={styles.catalogContentContainer}>
+          <div style={{position:"relative"}}>
+            <div className={styles.catalogCover}/>
+          </div>
+          <div className={styles.catalogContent}>
+            <div className={styles.catalogColumn}>
+              <Image src={Bvexterior} objectFit="cover" alt="bvexterior" className={styles.catalogImage}/>
+              <Image src={Mansion} objectFit="cover" alt="truka mansion" className={styles.catalogImage}/>
+            </div>
+            <Image src={Dwiw} objectFit="cover" alt="dwiw" className={styles.catalogImageLarge}/>
+            <div className={styles.catalogColumn}>
+              <Image src={Poker} objectFit="cover" alt="bvexterior" className={styles.catalogImage}/>
+              <div className={styles.catalogRow}>
+                <Image src={Peakingduck} objectFit="cover" alt="peakingduck" className={styles.catalogImageSmall}/>
+                <Image src={Truka} objectFit="cover" alt="truka" className={styles.catalogImageSmall}/>
+              </div>
+            </div>
+            <div className={styles.catalogColumn}>
+              <Image src={Tradesmark} objectFit="cover" alt="tradesmark" className={styles.catalogImage}/>
+              <Image src={Blackprint} objectFit="cover" alt="blackprint" className={styles.catalogImage}/>
+            </div>
+          </div>
+       
+        </div>
+        
       </section>
     </main>
   );
